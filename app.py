e6fe212e8744e8c9a4cdbbef1dbd2a0fd06ec642
Travis Harry <travisdharry@gmail.com>
2022-11-04 11:19:52 -0500

    cleaned up app.py

--- app.py
+++ app.py
@@ -21,8 +21,6 @@ import plotly
 # Internal imports
 from ffpackage.scraping import mfl
 from ffpackage.analysis import analysis
-# from ffpackage.scraping import ffdb
-# from ffpackage.scraping import ourlads
 from ffpackage.viz import viz
 from appmanager.database import db
 from appmanager.user import user
@@ -123,47 +121,63 @@ def callback():
         # Send user back to index page
         return redirect(url_for("index"))
 
+
+
+### Landing pages
+
 @app.route('/landing')
 #@login_required
-def getLeague():
-    return render_template("landing.html")
+def landing():
+    # Check if the user has entered a league and franchise
+    user_franchise = session.get('user_franchise', None)
+    user_league = session.get('user_league', None)
+    return render_template("landing.html", user_league=user_league, user_franchise=user_franchise)
 
 @app.route("/landing/leagueCallback", methods=['GET', 'POST'])
 #@login_required
 def leagueCallback():
+    # The user will input their leagueID into the site
     user_league = request.form["user_league"]
+    # Save the user's leagueID in the session data
     session['user_league'] = user_league
+    # Send the user to the getFrnachise page to enter their franchise
     return redirect(url_for("getFranchise"))
 
 @app.route('/getFranchise', methods=['GET', 'POST'])
 #@login_required
 def getFranchise():
+    # Retrieve the user's league they entered on the getLeague page
     user_league = session.get("user_league")
-
-    franchises = get_league(user_league)
-
-    return render_template("getFranchise.html", franchise_list=data)
+    # Find the franchises that are in the user's reported league; these will go into a dropdown menu for the user
+    franchise_list = mfl.get_franchises(user_league)['franchiseName'].to_list()
+    # Render the page
+    return render_template("getFranchise.html", franchise_list=franchise_list)
 
 @app.route("/getFranchise/franchiseCallback", methods=['GET', 'POST'])
 #@login_required
 def franchiseCallback():
+    # Retrieve the user's franchise they entered on the getFranchise page
     user_franchise = request.form["FranchiseName"]
+    # Save the user's franchiseName in the session data
     session['user_franchise'] = user_franchise
+    # return the user to the landing page
     return redirect(url_for("landing"))
 
-@app.route('/landing')
-#@login_required
-def landing():
-    user_franchise = session.get('user_franchise', None)
-    user_league = session.get('user_league', None)
-    return render_template("landing.html", user_league=user_league, user_franchise=user_franchise)
+
+
+
+### Views
+
 
 @app.route("/allPlayers")
 #@login_required
 def allPlayers():
+    # get all player info that is stored in the app's database
     player_df = db.get_df("player_df")
+    # convert to html tables
     tables = [player_df.to_html(classes='data')]
     titles=player_df.columns.values
+    # Render html
     return render_template("allPlayers.html", tables=tables, titles=titles)
 
 @app.route('/waiverWire', methods=['GET', 'POST'])
@@ -173,110 +187,88 @@ def waiverWire():
     user_franchise = session.get('user_franchise', None)
 
     # Get Franchises in the league
-    urlString = f"https://www54.myfantasyleague.com/2022/export?TYPE=league&L={user_league}"
-    response = requests.get(urlString)
-    soup = BeautifulSoup(response.content,'xml')
-    data = []
-    franchises = soup.find_all('franchise')
-    for i in range(len(franchises)):
-        rows = [franchises[i].get("id"), franchises[i].get("name")]
-        data.append(rows)
-    franchise_df = pd.DataFrame(data)
-    franchise_df.columns=['FranchiseID','FranchiseName']
-    franchise_df = franchise_df.append({"FranchiseID":"FA", "FranchiseName":"Free Agent"}, ignore_index=True)
+    franchise_df = mfl.get_franchises(user_league)
+    # Append a row to carry free agents
+    franchise_df = franchise_df.append({"franchiseID":"FA", "franchiseName":"Free Agent"}, ignore_index=True)
 
     # Get franchise rosters
-    urlString = f"https://www54.myfantasyleague.com/2022/export?TYPE=rosters&L={user_league}&FRANCHISE={user_franchise}"
-    response = requests.get(urlString)
-    soup = BeautifulSoup(response.content,'xml')
-    data = []
-    franchises = soup.find_all('franchise')
-    for i in range(0,len(franchises)):
-        current_franchise = franchises[i].find_all('player')
-        for j in range(0,len(current_franchise)):
-            rows = [franchises[i].get("id"), franchises[i].get("week"), current_franchise[j].get("id"), current_franchise[j].get("status")]
-            data.append(rows)
-    rosters_df = pd.DataFrame(data)
+    rosters_df = mfl.get_rosters(user_league, user_franchise=user_franchise)
 
     # Get Free Agents
-    urlString = f"https://www54.myfantasyleague.com/2022/export?TYPE=freeAgents&L={user_league}"
-    response = requests.get(urlString)
-    soup = BeautifulSoup(response.content,'xml')
-    data = []
-    freeAgents = soup.find_all('player')
-    for i in range(len(freeAgents)):
-        rows = ["FA", "", freeAgents[i].get("id"), "Free Agent"]
-        data.append(rows)
-    fa_df = pd.DataFrame(data)
-    rosters_df = rosters_df.append(fa_df)
-    rosters_df.columns=['FranchiseID','Week','PlayerID','RosterStatus']
+    freeAgent_df = mfl.get_freeAgents(user_league)
 
-    # Get all players, sharkRank, and ADP
-    player_df = get_df("predictions")
+    # Combine Franchise rosters with free agents to get all players
+    rosters_df = pd.concat([rosters_df, freeAgent_df], axis=0)
+
+    # Get all players, sharkRank, and ADP from the app's database
+    player_df = db.get_df("predictions")
 
     # Merge all dfs
-    complete = player_df.merge(rosters_df, left_on='id_mfl', how='left', right_on='PlayerID').merge(franchise_df[['FranchiseID', 'FranchiseName']], on='FranchiseID', how='left')
-    complete = complete[complete['FranchiseID'].notna()]
-    complete = complete.sort_values(by=['pred'], ascending=False)
-    complete.reset_index(inplace=True, drop=True)
-    complete = complete[['player', 'age', 'team', 'FranchiseName', 'pos', 'posRank', 'KR', 'PR', 'RES', 'pred', 'sharkAbsolute', 'adpAbsolute']]
-    complete.rename(columns={
+    waiverPlayers = player_df.merge(rosters_df, on='id_mfl', how='left').merge(franchise_df[['franchiseID', 'franchiseName']], on='franchiseID', how='left')
+    # Select only players who are not already on a franchise, i.e., are on the waiver wire
+    waiverPlayers = waiverPlayers[waiverPlayers['franchiseID'].notna()]
+    # Sort players by season point prediction
+    waiverPlayers = waiverPlayers.sort_values(by=['pred'], ascending=False)
+    waiverPlayers.reset_index(inplace=True, drop=True)
+    # Format the table
+    waiverPlayers = waiverPlayers[['player', 'age', 'team', 'franchiseName', 'pos', 'posRank', 'KR', 'PR', 'RES', 'pred', 'sharkAbsolute', 'adpAbsolute']]
+    waiverPlayers.rename(columns={
         'player':'Player',
         'age':'Age',
         'team':'Team',
         'pos':'Position',
         'posRank': 'Rank',
-        'pred': 'ChopBlock Prediction',
+        'pred': 'My Prediction',
         'sharkAbsolute': 'FantasySharks Prediction',
         'adpAbsolute': 'ADP-Based Prediction'
     }, inplace=True)
-    complete.set_index('Player', drop=True, inplace=True)
-
-    return render_template("waiverWire.html", tables=[complete.to_html(classes='data')], titles=complete.columns.values)
+    waiverPlayers.set_index('Player', drop=True, inplace=True)
+    
+    # Render html
+    return render_template("waiverWire.html", tables=[waiverPlayers.to_html(classes='data')], titles=waiverPlayers.columns.values)
 
 
 #@app.route('/compareFranchises')
 #@login_required
 def compareFranchises():
+    # Retrieve the user's leagueID from the session data
     user_league = session.get('user_league', None)
 
     # Get Franchises in the league
     franchise_df = mfl.get_franchises(user_league)
-    # Append a franchise for free agents
-    franchise_df = franchise_df.append({"franchiseID":"FA", "franchiseName":"Free Agent", "franchiseAbbrev":"FA"}, ignore_index=True)
-
+    # Append a franchise for free agents (Removed this feature because did not want to look at Free Agents in this view)
+    # franchise_df = franchise_df.append({"franchiseID":"FA", "franchiseName":"Free Agent", "franchiseAbbrev":"FA"}, ignore_index=True)
     # Get franchise rosters
-    rosters_df = mfl.get_rosters(user_league)
-
-    # Get Free Agents
-    freeAgent_df = mfl.get_freeAgents(user_league)
-
+    rosteredPlayers = mfl.get_rosters(user_league)
     # Get all players, sharkRank, and ADP
     predictions = db.get_df("predictions")
 
     # Merge all dfs
-    complete = predictions.merge(rosters_df, on='id_mfl', how='left').merge(franchise_df[['franchiseID', 'franchiseName', 'franchiseAbbrev']], on='franchiseID', how='left')
-    complete['franchiseID'].fillna("FA", inplace=True)
-    complete['franchiseName'].fillna("Free Agent", inplace=True)
-    complete['rosterStatus'].fillna("Free Agent", inplace=True)
+    rosteredPlayers = predictions.merge(rosteredPlayers, on='id_mfl', how='left').merge(franchise_df[['franchiseID', 'franchiseName', 'franchiseAbbrev']], on='franchiseID', how='left')
+    rosteredPlayers['franchiseID'].fillna("FA", inplace=True)
+    rosteredPlayers['franchiseName'].fillna("Free Agent", inplace=True)
+    rosteredPlayers['rosterStatus'].fillna("Free Agent", inplace=True)
 
     # Get info on available slots from MFL site
+    # For now the numbers are hard-coded
     posMax = {"QB":2, "RB":5, "WR":6, "TE":5, "PK":2, "DF":2}
     posMin = {"QB":1, "RB":2, "WR":2, "TE":2, "PK":2, "DF":2}
     totalStarters = 15
+    # The user will select a prediction method (ADP, FantasySharks, this app, etc.)
+    # For now the method is hard-coded to this app's predictions
     predMethod = "pred"
 
     # Select starters
-    df = analysis.starterSelector(complete, how=predMethod, startersMax=totalStarters, posMax=posMax, posMin=posMin)
+    startingPlayers = analysis.starterSelector(rosteredPlayers, how=predMethod, startersMax=totalStarters, posMax=posMax, posMin=posMin)
 
     # Find the lowest scoring player on the field and set them as the low bar
     for x in ["QB", "RB", "WR", "TE", "PK", "DF"]:
-        positionMinPred = df.loc[(df['pos']==x) & (df['starting']=='Starter'), predMethod].min()
+        positionMinPred = startingPlayers.loc[(startingPlayers['pos']==x) & (startingPlayers['starting']=='Starter'), predMethod].min()
         # Calculate relative values
-        df.loc[df['pos']==x, 'relativeValue'] = df.loc[df['pos']==x, predMethod] - positionMinPred
+        startingPlayers.loc[startingPlayers['pos']==x, 'relativeValue'] = startingPlayers.loc[startingPlayers['pos']==x, predMethod] - positionMinPred
 
     # Visualize the data
-    fig = viz.compareFranchises(df, how=predMethod)
+    fig = viz.compareFranchises(startingPlayers, how=predMethod)
     # Encode in JSON format
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     # Render html template in flask
@@ -285,6 +277,7 @@ def compareFranchises():
 @app.route('/liveScoring')
 #@login_required
 def liveScoring():
+    # Retrieve the user's leagueID from the session data
     user_league = session.get("user_league")
 
     # Get MFL scoring data
@@ -295,25 +288,25 @@ def liveScoring():
     predictions = db.get_df("predictions")
 
     # Merge: merge liveScores, franchises, and predictions
-    df = liveScores.merge(franchises, how='left', on='franchiseID').merge(predictions, how='left', on='id_mfl')
+    liveScores = liveScores.merge(franchises, how='left', on='franchiseID').merge(predictions, how='left', on='id_mfl')
 
     # Clean: convert to float data types
-    df['liveScore'] = df['liveScore'].astype('float64')
-    df['secondsRemaining'] = df['secondsRemaining'].astype('float64')
+    liveScores['liveScore'] = liveScores['liveScore'].astype('float64')
+    liveScores['secondsRemaining'] = liveScores['secondsRemaining'].astype('float64')
 
     # Filter:
-    df = df.loc[df['status']=="starter"]
+    liveScores = liveScores.loc[liveScores['status']=="starter"]
 
     # Analyze:
     # Divide the player's annual prediction by the number of weeks (17) to get a weekly prediction
-    df['weeklyPred'] = df['pred'] / 17
+    liveScores['weeklyPred'] = liveScores['pred'] / 17
     # Calculate each player's expected score at the end of the game
-    df['expectedLiveScore'] = df.apply(analysis.expectedLiveScore, axis=1)
+    liveScores['expectedLiveScore'] = liveScores.apply(analysis.expectedLiveScore, axis=1)
     # Set colors for chart 
-    df['color'] = df.apply(analysis.colorPicker, axis=1)
+    liveScores['color'] = liveScores.apply(analysis.colorPicker, axis=1)
 
     # Visualize:
-    fig = viz.liveScoring(df)
+    fig = viz.liveScoring(liveScores)
     # Encode in JSON format
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     # Render html template in flask
